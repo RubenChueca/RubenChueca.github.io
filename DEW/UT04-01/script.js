f118dad265346b1c9821aa7a8346ae7a859cfe40
Rubén José Chueca Fernández Trujillo <39775714+RubenChueca@users.noreply.github.com>
2024-11-29 12:19:19 +0000

    add error container

--- DEW/UT04-01/script.js
+++ DEW/UT04-01/script.js
@@ -1,12 +1,22 @@
+const DOM = {
+    form: document.querySelector('#frm'),
+    select_dni: document.querySelector('#select-dni'),
+    mostrarContra: document.querySelector('#mostrarContra'),
+    titulo: document.querySelector('#titulo'),
+    tituloCount: document.querySelector('#tituloCount'),
+    descripcion: document.querySelector('#descripcion'),
+    descripcionCount: document.querySelector('#descripcionCount')
+}
+
 // Deshabilitar campo de texto dni si no se selecciona una opción
-document.querySelector('#select-dni').addEventListener('change', () => {
+DOM.select_dni.addEventListener('change', () => {
     const dniInput = document.getElementById('dni');
     dniInput.disabled = this.value === "";
 });
 
 // Mostrar o ocultar contraseña
-document.querySelector("#mostrarContra").addEventListener("click", () => {
-    let inputPass = document.querySelector("#contrasenia");
+DOM.mostrarContra.addEventListener("click", () => {
+    let inputPass = document.querySelector("#contrasena");
 
     if (inputPass.type === "password") {
         inputPass.type = "text";
@@ -15,18 +25,20 @@ document.querySelector("#mostrarContra").addEventListener("click", () => {
     }
 })
 
-
 // Control de contador de caracteres
-document.querySelector("#titulo").addEventListener("input", () => {
+DOM.titulo.addEventListener("input", () => {
     let inputTitulo = document.querySelector("#titulo");
     let inputLength = inputTitulo.value.length;
 
-    document.querySelector("#tituloCount").textContent = inputLength;
+    DOM.tituloCount.textContent = inputLength;
 });
 
-document.querySelector("#descripcion").addEventListener("input", () => {
+DOM.descripcion.addEventListener("input", () => {
     let inputTitulo = document.querySelector("#descripcion");
     let inputLength = inputTitulo.value.length;
 
-    document.querySelector("#descripcionCount").textContent = inputLength;
+    DOM.descripcionCount.textContent = inputLength;
 });
+
+// Validación formulario
+DOM.form.addEventListener
