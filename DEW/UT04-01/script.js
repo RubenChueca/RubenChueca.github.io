4de1e1a2bdccaf54c64a28d7ab19518c65fa78bc
Rubén José Chueca Fernández Trujillo <39775714+RubenChueca@users.noreply.github.com>
2024-12-04 10:07:26 +0000

    cleaning code

--- DEW/UT04-01/script.js
+++ DEW/UT04-01/script.js
@@ -1,18 +1,23 @@
 // ------- Elementos del documento --------
 const DOM = {
+    layout: document.querySelector('.layout'),
     form: document.querySelector('#frm'),
-    inputs: document.querySelectorAll("input, textarea"),
-    select_dni: document.querySelector('#select-dni'),
-    dni: document.querySelector('#dni'),
+    allInputs: document.querySelectorAll("input, textarea"),
+    contrasena: document.querySelector('#contrasena'),
     mostrarContra: document.querySelector('#mostrarContra'),
+    select_dni: document.querySelector('#select-dni'),
+    IdDni: document.querySelector('#dni'),
+    classDni: document.querySelector('.dni'),
+    aficionesContainer: document.querySelector('.aficiones'),
+    aficionesCheckbox: document.querySelectorAll('.aficiones input[type="checkbox"]'),
+    aficiones: document.querySelector('#aficiones'),
     titulo: document.querySelector('#titulo'),
     tituloCount: document.querySelector('#tituloCount'),
     descripcion: document.querySelector('#descripcion'),
     descripcionCount: document.querySelector('#descripcionCount'),
-    aficiones: document.querySelector('#aficiones'),
-    errorContainer: document.querySelector('#error-container'),
-    contrasena: document.querySelector("#contrasena"),
-}
+    errorContainer: document.querySelector('.error-container'),
+    aficionesErr: document.querySelector("#aficionesErr"),
+};
 
 // -------- Mensajes de error personalizados --------
 const errorMessages = {
@@ -24,20 +29,32 @@ const errorMessages = {
     codPostal: "El código postal es obligatorio",
     dni: "El DNI / NIE es obligatorio",
     titulo: "El título de la publicación es obligatorio",
-    descripcion: "La descripción de la publicación es obligatoria"
-}
+    descripcion: "La descripción de la publicación es obligatoria",
+    aficiones: "Tienes que seleccionar al menos dos aficiones",
+};
+
+// -------- Crear contenedor errores por --------
+// -------- defecto de ValidationMessage --------
+(() => {
+    let errorContainer = document.createElement("div");
+    errorContainer.classList.add("error-container");
+    DOM.layout.appendChild(errorContainer);
+})();
+
 
 // -------- Habilitar campo dni cuando--------
 // -------- se seleccione una opción --------
 DOM.select_dni.addEventListener('change', () => {
     if (DOM.select_dni.value === 'dni') {
-        DOM.dni.disabled = false;
-        DOM.dni.setAttribute('pattern', '^\\d{8}[A-Za-z]$');
-        DOM.dni.placeholder = 'Ejem: 11122233A';
+        DOM.IdDni.disabled = false;
+        DOM.IdDni.focus();
+        DOM.IdDni.setAttribute('pattern', '^\\d{8}[A-Za-z]$');
+        DOM.IdDni.placeholder = 'Ejem: 11122233A';
     } else if (DOM.select_dni.value === 'nie') {
-        DOM.dni.disabled = false;
-        DOM.dni.setAttribute('pattern', '^[A-Za-z]\\d{7}[A-Za-z]$');
-        DOM.dni.placeholder = 'Ejem: A11122233B';
+        DOM.IdDni.disabled = false;
+        DOM.IdDni.focus();
+        DOM.IdDni.setAttribute('pattern', '^[A-Za-z]\\d{7}[A-Za-z]$');
+        DOM.IdDni.placeholder = 'Ejem: A11122233B';
     }
 });
 
@@ -55,42 +72,27 @@ DOM.mostrarContra.addEventListener("click", () => {
 
 // -------- Añadir clase validated cuando --------
 // -------- un input pierda el foco --------
-DOM.inputs.forEach((input) => {
+DOM.allInputs.forEach((input) => {
     input.addEventListener("blur", () => {
         input.classList.add("validated");
         handleValidation(input);
     });
 });
 
-function handleValidation(input) {
-    let errorSpan = document.querySelector(`#${input.id}Err`);
-    if (input.validity.valid) {
-        if (errorSpan) errorSpan.remove();
-    } else {
-        if (!errorSpan) {
-            errorSpan = document.createElement("span");
-            errorSpan.id = `${input.id}Err`;
-            errorSpan.style.color = "red";
-            errorSpan.textContent = errorMessages[input.id];
-            input.id !== "dni"
-                ? input.insertAdjacentElement("afterend", errorSpan)
-                : document.querySelector(".dni").insertAdjacentElement("beforeend", errorSpan);
-        }
-    }
-}
-
 // -------- Validación formulario --------
 DOM.form.addEventListener("submit", (e) => {
     let isValid = true;
     let aficionesChecked = checkAficiones();
     let err = [];
+    let inputs = [];
 
-    DOM.inputs.forEach((input) => {
+    DOM.allInputs.forEach((input) => {
         input.classList.add("validated");
 
         if (!input.validity.valid || input.disabled == true) {
             isValid = false;
-            err.push(input);
+            err.push(input.id);
+            inputs.push(input);
         }
     });
 
@@ -102,14 +104,36 @@ DOM.form.addEventListener("submit", (e) => {
 
     if (!isValid) {
         e.preventDefault();
-        createErrElements(err, aficionesChecked);
+
+        if (aficionesChecked.length <= 1) err.push("aficiones");
+        createErrElements(err);
+        createDefaultErrors(inputs, err);
     }
 });
+
+// -------- Método para crear mensajes de --------
+// -------- error cuando se pierda el foco --------
+function handleValidation(input) {
+    let errorSpan = document.querySelector(`#${input.id}Err`);
+    if (input.validity.valid) {
+        if (errorSpan) errorSpan.remove();
+    } else {
+        if (!errorSpan) {
+            errorSpan = document.createElement("span");
+            errorSpan.id = `${input.id}Err`;
+            errorSpan.classList.add("errorMessage");
+            errorSpan.textContent = errorMessages[input.id];
+            input.id !== "dni"
+                ? input.insertAdjacentElement("afterend", errorSpan)
+                : DOM.classDni.insertAdjacentElement("beforeend", errorSpan);
+        }
+    }
+}
 
 // -------- Método para comprobar si --------
 // -------- hay aficiones seleccionadas --------
 function checkAficiones() {
-    let aficionesInputs = document.querySelectorAll('.aficiones input[type="checkbox"]');
+    let aficionesInputs = DOM.aficionesCheckbox;
     let arrAficiones = [];
 
     aficionesInputs.forEach(input => {
@@ -122,32 +146,30 @@ function checkAficiones() {
 }
 
 // -------- Método para crear mensajes de error --------
-function createErrElements(err, aficionesChecked) {
-    // Limpiar contenido de errorContainer
-    err.forEach(({ id }) => {
-        let existingError = document.querySelector(`#${id}Err`);
+function createErrElements(err) {
+    err.forEach(errMessage => {
+        let existingError = document.querySelector(`#${errMessage}Err`);
         if (existingError) existingError.remove();
 
-        let span = document.createElement("span");
-        span.id = `${id}Err`;
-        span.style.color = "red";
-        span.textContent = errorMessages[id];
+        let errorSpan = document.createElement("span");
+        errorSpan.id = `${errMessage}Err`;
+        errorSpan.classList.add("errorMessage");
+        errorSpan.textContent = errorMessages[errMessage];
 
-        if (id != "dni") {
-            document.querySelector(`#${id}`).insertAdjacentElement("afterend", span);
+        if (errMessage != "dni") {
+            document.querySelector(`#${errMessage}`).insertAdjacentElement("afterend", errorSpan);
         } else {
-            document.querySelector('.dni').insertAdjacentElement("beforeend", span);
+            DOM.classDni.insertAdjacentElement("beforeend", errorSpan);
         }
     });
+}
 
-    if (aficionesChecked.length <= 1) {
-        let existingError = document.querySelector("#aficionesErr");
-        if (existingError) existingError.remove();
-
-        let span = document.createElement("span");
-        span.id = "aficionesErr";
-        span.style.color = "red";
-        span.textContent = "Tienes que seleccionar al menos dos aficiones";
-        document.querySelector('.aficiones').insertAdjacentElement("beforeend", span);
-    }
+// -------- Método para crear mensajes de error por defecto --------
+function createDefaultErrors(inputs, err) {
+    inputs.forEach(input => {
+        let errorSpan = document.createElement("span");
+        errorSpan.classList.add("errorMessage");
+        errorSpan.textContent = input.validationMessage;
+        DOM.errorContainer.insertAdjacentElement("beforeend", errorSpan);
+    })
 }
